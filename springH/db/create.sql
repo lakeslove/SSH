737e450837a7ea9ef956ede2c86b1c498f152328
lakeslove <lakeslove@126.com>
2017-01-24 09:00:40 +0800

    添加数据库文件

--- springH/db/create.sql
+++ springH/db/create.sql
@@ -1,1 +1,6 @@
 #mysql数据库
+create database mybatis;
+use mybatis;
+CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(20), age INT);
+INSERT INTO users(NAME, age) VALUES('孤傲苍狼', 27);
+INSERT INTO users(NAME, age) VALUES('白虎神皇', 27);
