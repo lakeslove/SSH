3f3540bf2e4b641f99d77b8c25b60d6758aaa0bf
lakeslove <lakeslove@126.com>
2017-02-14 02:09:07 +0800

    springIOC和初始化配置的检查

--- springH/src/main/com/lx/utils/CheckConfigs.java
+++ springH/src/main/com/lx/utils/CheckConfigs.java
@@ -3,7 +3,6 @@ package com.lx.utils;
 import java.io.File;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
@@ -11,21 +10,21 @@ import org.hibernate.Transaction;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+
 import com.lx.daos.UserDao;
+import com.lx.daos.hibernate.UserDaoImpl;
 
 public class CheckConfigs implements ApplicationContextAware {
 	private static final Logger log = LogManager.getLogger(CheckConfigs.class);
 	private static final Logger sysLog = LogManager.getLogger("systemlog");
-	private static final String TSS_KZ_AUDIT = "TSS_KZ_AUDIT";
-	private static final String[] wellnessEnvironmentVariables = new String[]{"TSS_KZ_LOGS",TSS_KZ_AUDIT};
-	private static String jndiName;
+	private static String dbUrl;
 	
-	public static void setJndiName(String jndiName) {
-		CheckConfigs.jndiName = jndiName;
+	public static void setDbUrl(String dbUrl) {
+		CheckConfigs.dbUrl = dbUrl;
 	}
 
 	private UserDao userDao;
-	public void setSearchServiceUserDao(UserDao userDao) {
+	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
 
@@ -41,41 +40,6 @@ public class CheckConfigs implements ApplicationContextAware {
 	
 	private void checkEnvironmentVariable(){
 		Map<String,String> environmentVariables = System.getenv();
-		for(String varibale:wellnessEnvironmentVariables){
-			if(!environmentVariables.containsKey(varibale)){
-				String errorMessage = PropertyUtil.getPropertyValue("errors.environmentVaribale.noExist");
-				String logMessage = errorMessage.replace("{1}", varibale);
-				sysLog.fatal(logMessage);
-				log.fatal(logMessage);
-				continue;
-			}
-			if(StringUtils.isEmpty(environmentVariables.get(varibale))){
-				String errorMessage = PropertyUtil.getPropertyValue("errors.environmentVaribale.value.noExist");
-				String logMessage = errorMessage.replace("{1}", varibale);
-				sysLog.fatal(logMessage);
-				log.fatal(logMessage);
-				continue;
-			}
-			if(!validateFilePath(environmentVariables.get(varibale))){
-				String errorMessage = PropertyUtil.getPropertyValue("errors.environmentVaribale.filePath.noExist");
-				String logMessage = errorMessage.replace("{1}", varibale).replace("{2}", environmentVariables.get(varibale));
-				sysLog.fatal(logMessage);
-				log.fatal(logMessage);
-			}
-		}
-		if(environmentVariables.containsKey(TSS_KZ_AUDIT)){
-			String flagIsAdmin = PropertyUtil.getPropertyValue("flag.isAdmin");
-			if("all".equals(flagIsAdmin) || "true".equals(flagIsAdmin)) {
-			    String csvSubPath = PropertyUtil.getPropertyValue("medicalCheckup.download.filepath");
-			    String auditPath = csvSubPath.replace("${TSS_KZ_AUDIT}", environmentVariables.get(TSS_KZ_AUDIT));
-			    if(!validateFilePath(auditPath)){
-			        String errorMessage = PropertyUtil.getPropertyValue("errors.filePath.noExist");
-			        String logMessage = errorMessage.replace("{1}", auditPath);
-			        sysLog.fatal(logMessage);
-			        log.fatal(logMessage);
-			     }
-			 }
-		}
 	}
 	
 	private boolean validateFilePath(String path){
@@ -93,10 +57,6 @@ public class CheckConfigs implements ApplicationContextAware {
 			Transaction t = session.beginTransaction();
 			t.commit();
 		}catch (Exception e) {
-			String errorMessage = PropertyUtil.getPropertyValue("errors.jndi.notConnect");
-			String logMessage = errorMessage.replace("{1}", jndiName);
-			sysLog.fatal(logMessage);
-			log.fatal(logMessage);
 			log.error(e.getMessage(), e);
 		}
 	}
